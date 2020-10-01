b713b1ab1e50aad999824ea1f5a2d8462d18ee56
shuchitaj <36403093+shuchitaj@users.noreply.github.com>
2020-10-01 14:20:40 +0530

    Update run-bg-deploy.sh

--- run-bg-deploy.sh
+++ run-bg-deploy.sh
@@ -9,10 +9,10 @@ kubectl create cm tokens-configmap --from-file=misc/tokens.json
 kubectl create -f pvcs/shared-workspaces.yaml
 
 # installing tasks from upstream catalog
-kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/git/git-clone.yaml
-kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/replace-tokens/replace-tokens.yaml
+kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-clone/0.2/git-clone.yaml
+kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/replace-tokens/0.1/replace-tokens.yaml
 kubectl create -f https://raw.githubusercontent.com/vinamra28/catalog/vinamra28/sonar-fix/sonarqube/sonarqube.yaml
-kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/buildah/buildah.yaml
+kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/buildah/0.1/buildah.yaml
 kubectl create -f tasks/blue-green-deploy.yaml
 # installing the pipeline
 kubectl create -f pipelines/deployment-pipeline.yaml
