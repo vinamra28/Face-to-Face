4a9a7337ad2105e3be2b0fec1c96cea04b957463
Divyanshu Agrawal <diagrawa@redhat.com>
2020-10-01 10:42:10 +0530

    Fix broken link of tasks in delete script

--- delete-scripts.sh
+++ delete-scripts.sh
@@ -5,7 +5,7 @@ kubectl delete -f creds/openshift-install.yaml
 # 2.delete the pvc which will contain the cluster artifacts after the cluster is provisioned so that it can be used later
 kubectl delete -f pvcs/openshift-install-pvc.yaml
 # 3. install the task from the upstream catalog
-kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/openshift-provision/openshift-cluster-create.yaml
+kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/openshift-install/0.1/openshift-install.yaml
 # .4 delete and start the pipelinerun to provision the cluster on aws
 kubectl delete -f pipelines/provision-cluster-pipeline.yaml
 
@@ -17,12 +17,12 @@ kubectl delete -f creds/serviceaccount.yaml
 kubectl delete cm tokens-configmap
 kubectl delete -f pvcs/shared-workspaces.yaml
 
-# installing tasks from upstream catalog
-kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/git/git-clone.yaml
-kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/replace-tokens/replace-tokens.yaml
+# deleting tasks
+kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-clone/0.1/git-clone.yaml
+kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/replace-tokens/0.1/replace-tokens.yaml
 # kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/sonarqube/sonarqube.yaml
 kubectl delete -f https://raw.githubusercontent.com/vinamra28/catalog/vinamra28/sonar-fix/sonarqube/sonarqube.yaml
-kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/buildah/buildah.yaml
+kubectl delete -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/buildah/0.1/buildah.yaml
 kubectl delete -f tasks/blue-green-deploy.yaml
 # installing the pipeline
 kubectl delete -f pipelines/deployment-pipeline.yaml
